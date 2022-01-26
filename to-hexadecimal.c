52a8df7a64852eec5da19f8f10617dcb7fcf7256
Siddharth Shankar <siddharth.shankar535@gmail.com>
2022-01-26 16:53:44 +0530

    Added to-hexadecimal.c file locally
    to-hexadecimal.c file prints the hexadecimal equivalent of a decimal number

---
+++ to-hexadecimal.c
@@ -0,0 +1,44 @@
+// THIS PROGRAM TAKES A DECIMAL NUMBER AS INPUT AND PRINTS IT'S HEXADECIMAL EQUIVALENT
+
+#include<stdio.h>
+#define MAX 9
+
+int main() {
+
+	char hexa[MAX] = {'0'};
+	int number = 0,temp = 0,last = MAX-2,rem = 0;
+	hexa[8] = '\0';
+	printf("Enter a positive integer : ");
+	scanf("%d",&number);
+
+	// CALCULATING THE HEXADECIMAL OF THE ENTERED INTEGER
+	temp = number;
+	while(temp != 0) {
+	
+		rem = temp % 16;
+		if(rem < 10) {
+		
+			hexa[last] = 48 + rem;
+		}
+		else {
+		
+			hexa[last] = 55 + rem;
+		}
+		temp = temp / 16;
+		last--;
+
+	}
+
+	printf("Hexadecimal of %d is ",number);
+	//printf("%s\n",hexa);
+
+	last = 0;
+	while(last < MAX) {
+	
+		printf("%c",hexa[last]);
+		last++;
+	}
+	printf("\n");
+
+	return 0;
+}
